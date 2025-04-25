4c4b627224f65d73540213b4857b21215eb16b41
nipirennipi <leeyj@cug.edu.cn>
2025-04-25 11:25:19 +0800

    add shell script

---
+++ baseline/create_embeddings.sh
@@ -0,0 +1,10 @@
+VERSION=baseline_plus
+STATE=offline
+EMBEDDINGS_DIR="/data/lyjiang/RecSys_Challenge_2025/submit/${VERSION}/${STATE}"
+
+mkdir -p "${EMBEDDINGS_DIR}"
+
+python -m aggregated_features_baseline.create_embeddings \
+    --data-dir /data/lyjiang/RecSys_Challenge_2025 \
+    --embeddings-dir "${EMBEDDINGS_DIR}"
+    
